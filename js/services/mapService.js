b6f73f401c9aa3250da9ce531dfb58967f6c0b21
Dana Dubrovsky <danadubrov@hotmail.com>
2020-10-28 18:09:43 +0200

    good functions

--- js/services/mapService.js
+++ js/services/mapService.js
@@ -1,14 +1,20 @@
-import { mapUtils } from './services/utils.js'
+import { mapUtils } from './utils.js'
+import { mapStorage } from './localStorage.js'
 
 export const mapService = {
     getLocs,
+    addPlace,
     createPlaces,
     createPlace,
-    addPlace
+    getLocations,
+    deletePlace,
+    getPlaceIdxById,
+    getPlaceById
 }
 
 const STORAGE_KEY_PLACES = 'placesDB'
 var locs = [{ lat: 11.22, lng: 22.11 }]
+var gPlaces = [];
 
 function getLocs() {
     return new Promise((resolve, reject) => {
@@ -19,7 +25,7 @@ function getLocs() {
 }
 
 function createPlaces() {
-    var places = loadFromStorage(STORAGE_KEY_PLACES);
+    var places = mapStorage.loadFromStorage(STORAGE_KEY_PLACES);
     if (places) gPlaces = places;
     else gPlaces = [];
 }
@@ -38,6 +44,30 @@ function createPlace(location, name) {
 
 function addPlace(location, placeName) {
     gPlaces.push(createPlace(location, placeName));
-    saveToStorage(STORAGE_KEY_PLACES, gPlaces);
+    mapStorage.saveToStorage(STORAGE_KEY_PLACES, gPlaces);
 
+}
+
+function deletePlace(placeId) {
+    var placeIdx = getPlaceIdxById(placeId);
+    gPlaces.splice(placeIdx, 1)
+    mapStorage.saveToStorage(STORAGE_KEY_PLACES, gPlaces)
+}
+
+function getPlaceById(placeId) {
+    const place = gPlaces.find(place => {
+        return placeId === place.id;
+    });
+    return place;
+}
+
+function getPlaceIdxById(placeId) {
+    const placeIdx = gPlaces.findIndex(place => {
+        return placeId === place.id;
+    });
+    return placeIdx;
+}
+
+function getLocations() {
+    return gPlaces;
 }
